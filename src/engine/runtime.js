6aebf22158cc42401fbd97e1fdd9996e2f51a04e
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-02-11 20:24:32 +0000

    Text Dropdowns (vm Part) (#1)
    Exposes the new textdropdowns to extensions.
    
    To do: Find a logical way to implement number dropdowns.

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1219,14 +1219,15 @@ class Runtime extends EventEmitter {
                 type: menuId,
                 inputsInline: true,
                 output: 'String',
-                colour: categoryInfo.color1,
-                colourSecondary: categoryInfo.color2,
-                colourTertiary: categoryInfo.color3,
+                colour: menuInfo.acceptText ? '#FFFFFF' : categoryInfo.color1,
+                colourSecondary: menuInfo.acceptText ? '#FFFFFF' : categoryInfo.color2,
+                colourTertiary: menuInfo.acceptText ? '#FFFFFF' : categoryInfo.color3,
                 outputShape: menuInfo.acceptReporters ?
                     ScratchBlocksConstants.OUTPUT_SHAPE_ROUND : ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE,
                 args0: [
-                    {
-                        type: 'field_dropdown',
+                    {// to do: we could reimplement field_numberdropdown here really easily
+                        type: menuInfo.acceptText ?
+                            'field_textdropdown' : 'field_dropdown',
                         name: menuName,
                         options: menuItems
                     }
