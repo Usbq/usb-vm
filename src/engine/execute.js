f0830c584b40322c5488823abad5e46dee5a2ed7
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-02-13 17:10:48 +0000

    variableTypes > variableType

--- src/engine/execute.js
+++ src/engine/execute.js
@@ -311,7 +311,7 @@ class BlockCached {
 
         // Store the static fields onto _argValues.
         for (const fieldName in fields) {
-            if (typeof fields[fieldName].variableTypes !== 'undefined') {
+            if (typeof fields[fieldName].variableType !== 'undefined') {
                 this._argValues[fieldName] = {
                     id: fields[fieldName].id,
                     name: fields[fieldName].value
