98d5ac0ef5f5ed1956abed83273f8a5e15b2fab2
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-02-16 20:27:49 +0000

    expose some more utils

---
+++ src/util/usb-util.js
@@ -0,0 +1,23 @@
+const Base64Util = require('../util/base64-util');
+const Cast = require('../util/cast');
+const Clone = require('../util/clone');
+const Color = require('../util/color');
+const MathUtil = require('../util/math-util');
+const StringUtil = require('../util/string-util');
+const Timer = require('../util/timer');
+const uid = require('../util/uid');
+const xmlEscape = require('../util/xml-escape');
+
+const Util = {
+  Base64Util,
+  Cast,
+  Clone,
+  Color,
+  MathUtil,
+  StringUtil,
+  Timer,
+  uid,
+  xmlEscape
+}
+
+module.exports = Util;
