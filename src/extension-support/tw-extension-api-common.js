2906f32b34b097afebc256e69542a8ba340c39c3
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-02-16 20:28:37 +0000

    Update tw-extension-api-common.js

--- src/extension-support/tw-extension-api-common.js
+++ src/extension-support/tw-extension-api-common.js
@@ -2,12 +2,14 @@ const ArgumentType = require('./argument-type');
 const BlockType = require('./block-type');
 const TargetType = require('./target-type');
 const Cast = require('../util/cast');
+const Util = require('../util/usb-util');
 
 const Scratch = {
     ArgumentType,
     BlockType,
     TargetType,
-    Cast
+    Cast,
+    Util
 };
 
 module.exports = Scratch;
