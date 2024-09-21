18a91c9693ef42d7053e63798913b87ccc1ab6b2
Ashime Sho <a_2oo+gituseremail@outlook.com>
2024-09-21 12:33:18 -0400

    Fix obj ser for vars

--- src/serialization/sb3.js
+++ src/serialization/sb3.js
@@ -490,7 +490,8 @@ const serializeSound = function (sound) {
 const isVariableValueSafeForJSON = value => (
     typeof value === 'number' ||
     typeof value === 'string' ||
-    typeof value === 'boolean'
+    typeof value === 'boolean' ||
+    typeof value === 'object'
 );
 const makeSafeForJSON = value => {
     if (Array.isArray(value)) {
